3f4d9935d034a86751dc8730d9405600c11baf9d
ocanosoup <ocanosoup@gmail.com>
2018-01-05 14:29:51 +0000

    Styling

--- src/main/java/com/revature/controllers/AssignedReimbursementServlet.java
+++ src/main/java/com/revature/controllers/AssignedReimbursementServlet.java
@@ -78,12 +78,12 @@ public class AssignedReimbursementServlet extends HttpServlet {
         // Set response content type
   
       String html =  "<h1 align = \"center\">" + title + "</h1>\n" +
-                 "<table border = \"1\" align = \"center\">\n" +
+               "<table class=\"table table-hover table-striped table-bordered table-condensed\" border = \"1\" align = \"center\">\n" +
                     "<tr bgcolor = \"#949494\">\n"; 
         html+="<tr>\n<td>ReimbursementId</td><td>EmployeeId</td><td>GradingFormatId</td><td>Cost</td><td>SubmissionTime</td><td>TimeMissed</td><td>Justification</td><td>Urgent</td><td>Exceeding</td></tr>";
         for(Reimbursement reim : reimbursements) {
                 html+=("<tr>\n" + "<td>"+reim.getReimbursementId()+"</td><td>"+reim.getEmployeeId()+"</td><td>"+reim.getGradingFormatId()+"</td><td>"+ reim.getCost()+"</td><td>"+reim.getSubmissionTime()+"</td><td>"+reim.getTimeMissed()+"</td><td>"+reim.getJustification()+
-              "</td><td>" + reim.getUrgent() + "</td><td>" + reim.getExceeding() + "</td><td><form action=\"approveReimbursement\" method=\"post\"><input type=\"hidden\"name=\"reimbursementId\" value=\"" + reim.getReimbursementId() + "\"><input type=\"submit\" value=\"APPROVE\"></input></form></td><td><form action=\"denyReimbursement\" method=\"post\"><input type=\"hidden\"name=\"reimbursementId\" value=\"" + reim.getReimbursementId() + "\"><input type=\"submit\" value=\"DENY\"></input></form></td>\n</tr>\n");
+              "</td><td>" + reim.getUrgent() + "</td><td>" + reim.getExceeding() + "</td><td><form action=\"approveReimbursement\" method=\"post\"><input type=\"hidden\"name=\"reimbursementId\" value=\"" + reim.getReimbursementId() + "\"><button class=\"btn btn-success\">APPROVE</button></form></td><td><form action=\"denyReimbursement\" method=\"post\"><input type=\"hidden\"name=\"reimbursementId\" value=\"" + reim.getReimbursementId() + "\"><button class=\"btn btn-danger\">DENY</button></form></td><td><button class=\"btn btn-info disabled\">Info</button></td>\n</tr>\n");
         
         }
         html += "</table>\n</body>\n</html>";
